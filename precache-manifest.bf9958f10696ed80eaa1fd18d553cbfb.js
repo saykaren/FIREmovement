b5356f4a9218bed0c1185d1ad94d3ad19ca36bab
Karen Garcia <kgarcia@i3screen.com>
2019-12-07 23:00:56 -0700

    initial creation of app

---
+++ precache-manifest.bf9958f10696ed80eaa1fd18d553cbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db253f7739bf2d194cb1138b1e53d5f3",
+    "url": "/saykaren/FIREmovement/index.html"
+  },
+  {
+    "revision": "2b57b6f488def58799f1",
+    "url": "/saykaren/FIREmovement/static/css/main.aa8fbd82.chunk.css"
+  },
+  {
+    "revision": "a268326430ac15458d5f",
+    "url": "/saykaren/FIREmovement/static/js/2.d16061fe.chunk.js"
+  },
+  {
+    "revision": "2b57b6f488def58799f1",
+    "url": "/saykaren/FIREmovement/static/js/main.bcfd668a.chunk.js"
+  },
+  {
+    "revision": "350923d0d5c5003e6b94",
+    "url": "/saykaren/FIREmovement/static/js/runtime-main.d9d17c08.js"
+  },
+  {
+    "revision": "b50cce0187c48f11c1e0c5ca20f3227c",
+    "url": "/saykaren/FIREmovement/static/media/fireball.b50cce01.jpg"
+  }
+]);
